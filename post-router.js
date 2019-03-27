ffae2b64c9b9bb7b88065e66654158c6ec3cb688
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-27 15:29:26 -0500

    got delete and put requests working

--- post-router.js
+++ post-router.js
@@ -37,6 +37,68 @@ router.get('/:id', async (req, res) => {
             message: `Error getting post by id. Error: ${error}`
         })
     }
+}) 
+
+// POST post 
+
+router.post('/', async (req, res) => {
+    try {
+        const post = await PostDB.insert(req.body); 
+            res.status(201)
+            .json(post)
+    } catch (error) {
+        res.status(500).json({
+            message: `Cannot make post. Error: ${error}`
+        })
+    }
 })
+
+// PUT post 
+
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await PostDB.update(req.params.id, req.body); 
+        if (post) {
+        res.status(200)
+        .json(post)
+        } else {
+            res.status(404)
+            .json({
+                message: 'Could not find post to update'
+            })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Cannot update post. Error: ${error}`
+        })
+    }
+})
+
+
+// DELETE post 
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await PostDB.remove(req.params.id); 
+        if (post) {
+            res.status(200)
+            .json(post)
+        } else {
+            res.status(404)
+            .json({
+                message: 'could not find post to delete'
+            })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Could not delete post. Error: ${error}`
+        })
+    }
+})
+
+
+
 
 module.exports = router 
