0ee42b22290516c05ec5311ce35ae994cacd8f46
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-27 14:48:08 -0500

    get by id and post working

--- user-router.js
+++ user-router.js
@@ -3,7 +3,9 @@ const UserDB = require('./data/helpers/userDb.js');
 
 const router = express.Router(); 
 
-// api/users
+// api/users route 
+
+// Get users 
 router.get('/', async (req, res) => {
     try {
         const users = await UserDB.get(req.query); 
@@ -12,10 +14,43 @@ router.get('/', async (req, res) => {
     } catch (error) {
         res.status(500)
         .json({
-            message: 'Error retrieving users'
+            message: `Error retrieving users. Error: ${error}`
         })
     }
 } )
+
+// Get user by id 
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await UserDB.getById(req.params.id); 
+        if (user) {
+            res.status(200).json(user); 
+        } else {
+            res.status(404).json({ message: "user not found" })
+        }
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `Error getting user by id. Error: ${error}`
+        })
+    }
+}) 
+
+// Post user 
+
+router.post('/', async (req, res) => {
+    try {
+        const user = await UserDB.insert(req.body); 
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500)
+        .json({
+            message: `error adding the user. error: ${error}`
+        })
+    }
+}) 
+
+
 
 
 
